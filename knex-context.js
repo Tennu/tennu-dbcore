6efc776585a2947549cca550ca4345a3dc784c83
Victorio Berra <toryberra@gmail.com>
2015-12-03 17:13:38 +0000

    Added dbclient to notice output

--- knex-context.js
+++ knex-context.js
@@ -1,3 +1,5 @@
+var format = require('util').format;
+
 function knexContext(knex, migrationPath, client) {
     return MigrateAndSeed(knex, migrationPath, client).then(function() {
         return knex;
@@ -9,7 +11,7 @@ function MigrateAndSeed(knex, migrationPath, client) {
     return knex.migrate.latest({
         directory: migrationPath
     }).then(function() {
-        client._logger.notice('DBCore: database initialized.');
+        client._logger.notice(format('DBCore: %s database initialized.', client.config('database').client));
     });
 }
 
